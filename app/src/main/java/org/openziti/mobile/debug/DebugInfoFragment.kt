bb5979f32fdc3270221f00899af3ac8964baa61f
Eugene K <eugene.kobyakov@netfoundry.io>
2021-11-05 00:42:32 -0400

    fix crash on debug view rotation

--- app/src/main/java/org/openziti/mobile/debug/DebugInfoFragment.kt
+++ app/src/main/java/org/openziti/mobile/debug/DebugInfoFragment.kt
@@ -12,14 +12,13 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import org.openziti.mobile.databinding.FragmentDebugInfoBinding
-import org.openziti.util.DebugInfoProvider
 import java.io.PrintWriter
 import java.io.StringWriter
 
 /**
  * A placeholder fragment containing a simple view.
  */
-class DebugInfoFragment(val section: String, val provider: DebugInfoProvider) : Fragment() {
+class DebugInfoFragment() : Fragment() {
 
     private lateinit var pageViewModel: PageViewModel
     private var _binding: FragmentDebugInfoBinding? = null
@@ -30,12 +29,16 @@ class DebugInfoFragment(val section: String, val provider: DebugInfoProvider) : 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        pageViewModel = ViewModelProvider(this).get(PageViewModel::class.java).apply {
+
+        pageViewModel = ViewModelProvider(this).get(PageViewModel::class.java)
+
+        arguments?.getString(SECTION_ARG)?.let {
+            val provider = (activity as DebugInfoActivity).getSectionProvider(it)
             val text = StringWriter()
-            provider.runCatching { dump(section, text) }.onFailure {
+            runCatching { provider?.dump(it, text) }.onFailure {
                 it.printStackTrace(PrintWriter(text))
             }
-            setText(text.toString())
+            pageViewModel.setText(text.toString())
         }
     }
 
@@ -54,9 +57,15 @@ class DebugInfoFragment(val section: String, val provider: DebugInfoProvider) : 
     }
 
     companion object {
+        const val SECTION_ARG = "section"
+
         @JvmStatic
-        fun newInstance(section: String, provider: DebugInfoProvider): DebugInfoFragment {
-            return DebugInfoFragment(section, provider)
+        fun newInstance(section: String): DebugInfoFragment {
+            val args = Bundle().apply {
+                putString(SECTION_ARG, section)
+            }
+
+            return DebugInfoFragment().apply { arguments = args }
         }
     }
 
