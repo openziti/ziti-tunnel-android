7b1503ec1769e01551a5c64a67c972cacda955d9
Eugene K <eugene.kobyakov@netfoundry.io>
2023-08-10 13:08:29 -0400

    use LiveData instead of local broadcast

--- app/src/main/java/org/openziti/mobile/ZitiViewModel.kt
+++ app/src/main/java/org/openziti/mobile/ZitiViewModel.kt
@@ -4,38 +4,33 @@
 
 package org.openziti.mobile
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import org.openziti.Ziti
 import org.openziti.ZitiContext
 import org.openziti.util.NetworkStats
+import org.openziti.android.Ziti as ZitiAndroid
 
 /**
  *
  *
  */
 class ZitiViewModel: ViewModel() {
-    val contextData = MutableLiveData(Ziti.getContexts())
-    val networkStatsData = MutableLiveData<NetworkStats>()
-
-    val receiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            contextData.postValue(Ziti.getContexts())
-        }
+    private val contextData = MutableLiveData(Ziti.getContexts())
+    private val networkStatsData = MutableLiveData<NetworkStats>()
+    private val events = ZitiAndroid.identityEvents()
+    private val observer = Observer<Any> {
+        contextData.postValue(Ziti.getContexts())
     }
 
     init {
-        LocalBroadcastManager.getInstance(org.openziti.android.Ziti.app).registerReceiver(receiver,
-                IntentFilter().apply {
-                    addAction(org.openziti.android.Ziti.IDENTITY_ADDED)
-                    addAction(org.openziti.android.Ziti.IDENTITY_REMOVED)
-                })
+        events.observeForever(observer)
+    }
+
+    override fun onCleared() {
+        events.removeObserver(observer)
     }
 
     fun contexts(): LiveData<Collection<ZitiContext>> = contextData
