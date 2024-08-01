cd0be0226b12cffe0b4caf9e46bbcaa57e415c09
eugene <eugene.kobyakov@netfoundry.io>
2024-08-01 16:23:51 -0400

    set PKG_CONFIG_PATH for platform binaries

--- tunnel/build.gradle.kts
+++ tunnel/build.gradle.kts
@@ -45,7 +45,7 @@ android {
     externalNativeBuild {
         cmake {
             path("src/main/cpp/CMakeLists.txt")
-            version = "3.22.1"
+            version = properties["cmake.version"].toString()
         }
     }
     compileOptions {
