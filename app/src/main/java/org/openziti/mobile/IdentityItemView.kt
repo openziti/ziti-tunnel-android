b072f84674363e14b9081ac9a9d5c7cf74b05bab
eugene <eugene.kobyakov@netfoundry.io>
2025-01-06 15:26:33 -0500

    cleanup UI

--- app/src/main/java/org/openziti/mobile/IdentityItemView.kt
+++ app/src/main/java/org/openziti/mobile/IdentityItemView.kt
@@ -3,10 +3,13 @@
  */
 
 package org.openziti.mobile
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import org.openziti.mobile.databinding.IdentityitemBinding
 import org.openziti.mobile.model.Identity
 
@@ -15,27 +18,22 @@ import org.openziti.mobile.model.Identity
  */
 class IdentityItemView(context: Context) : RelativeLayout(context) {
 
-    private var _count: Int = 0
-    private var _isOn: Boolean = false
-    lateinit var ctxModel: Identity
-    val owner = context as AppCompatActivity
-    val binding: IdentityitemBinding
-
-    var isOn:Boolean
-        get() = this._isOn
-        set(value) {
-            this._isOn = value
-            binding.IdToggleSwitch.isChecked = value
-            binding.StatusLabel.text = if(value) "enabled" else "disabled"
-        }
+    private lateinit var ctxModel: Identity
+    private val owner = context as AppCompatActivity
+    private val binding: IdentityitemBinding
+    private val offline: Drawable
+    private val bubble: Drawable
 
     init {
         val a = context.obtainStyledAttributes(null, R.styleable.IdentityItemView, 0, 0)
         binding = IdentityitemBinding.inflate(LayoutInflater.from(context), this, true)
-        // LayoutInflater.from(context).inflate(R.layout.identityitem, this, true)
+
+        offline = ResourcesCompat.getDrawable(context.resources, R.drawable.offline, null)!!
+        bubble = ResourcesCompat.getDrawable(context.resources, R.drawable.bubble, null)!!
         a.recycle()
     }
 
+    @SuppressLint("DefaultLocale")
     fun setModel(ztx: Identity) {
         ztx.refresh()
 
@@ -48,16 +46,39 @@ class IdentityItemView(context: Context) : RelativeLayout(context) {
             binding.IdentityName.text = it
         }
 
+        ctxModel.authState().observe(owner) {
+            when (it) {
+                Identity.AuthNone -> {
+                    binding.ServiceCountBubble.setImageDrawable(offline)
+                    binding.ServicesStatus.text = context.getString(R.string.offline)
+                    binding.ServiceCount.text = ""
+                }
+                Identity.Authenticated -> {
+                    binding.ServicesStatus.setText(R.string.services)
+                    binding.ServiceCountBubble.setImageDrawable(bubble)
+                    ctxModel.services().observe(owner) { services ->
+                        binding.ServiceCount.text = String.format("%d", services.size)
+                    }
+                }
+                is Identity.AuthJWT -> {
+                    binding.ServiceCountBubble.setImageDrawable(offline)
+                    binding.ServicesStatus.text = context.getString(R.string.ext_login)
+                    binding.ServiceCount.text = ""
+                }
+            }
+        }
+
         ctxModel.services().observe(owner) {
             binding.ServiceCount.text = String.format("%d", it.size)
         }
         ctxModel.enabled().observe(owner) { state ->
-            isOn = state
+            binding.IdToggleSwitch.isChecked = state
+            binding.IdToggleSwitch.text = if(state) "enabled" else "disabled"
+            binding.StatusLabel.text = if(state) "enabled" else "disabled"
         }
 
         binding.IdToggleSwitch.setOnCheckedChangeListener { _, state ->
             ctxModel.setEnabled(state)
         }
-
     }
 }
