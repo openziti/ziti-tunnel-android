090150bcf354d7219ce72c7875c39423ad91529d
eugene <eugene.kobyakov@netfoundry.io>
2025-04-11 17:49:51 -0400

    add enrollment with URL

--- app/src/main/java/org/openziti/mobile/ZitiEnrollmentActivity.kt
+++ app/src/main/java/org/openziti/mobile/ZitiEnrollmentActivity.kt
@@ -6,7 +6,6 @@ package org.openziti.mobile
 
 import android.app.Activity
 import android.content.Intent
-import android.hardware.SensorManager
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
@@ -17,13 +16,51 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
 import com.google.zxing.BarcodeFormat
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanOptions
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import org.openziti.mobile.databinding.ActivityZitiEnrollmentBinding
+import java.net.URL
 
 
 class ZitiEnrollmentActivity : AppCompatActivity() {
@@ -46,15 +83,7 @@ class ZitiEnrollmentActivity : AppCompatActivity() {
             bar.setDisplayShowHomeEnabled(true)
             bar.setDisplayHomeAsUpEnabled(true)
         }
-/*
-        enroll_opts.setOnClickListener {
-            if (optsOpen) {
-                hideEnrollOptions()
-            } else {
-                showEnrollOptions()
-            }
-        }
-*/
+
         val scanLauncher = registerForActivityResult(ScanContract()) {
             if (it.contents == null) {
                 Toast.makeText(this@ZitiEnrollmentActivity, "Cancelled", Toast.LENGTH_LONG).show()
@@ -62,8 +91,6 @@ class ZitiEnrollmentActivity : AppCompatActivity() {
                 enroll(it.contents)
             }
         }
-        binding.QRButtonArea.setOnClickListener { qrEnroll(scanLauncher) }
-        binding.QRButton.setOnClickListener { qrEnroll(scanLauncher) }
 
         val jwtLauncher = registerForActivityResult(StartActivityForResult()) {
             if (it.resultCode == Activity.RESULT_OK) {
@@ -73,23 +100,29 @@ class ZitiEnrollmentActivity : AppCompatActivity() {
             }
         }
 
-        binding.JWTButton.setOnClickListener { jwtEnroll(jwtLauncher) }
-        binding.IdButton.setOnClickListener { jwtEnroll(jwtLauncher) }
+        binding.enrollmentContent.setContent {
+            val urlDialog = remember { mutableStateOf(false) }
+            if (urlDialog.value) {
+                URLDialog { url ->
+                    urlDialog.value = false
+                    url?.let { enroll(it) } ?: Toast.makeText(this@ZitiEnrollmentActivity, "Cancelled", Toast.LENGTH_LONG).show()
+                }
+            }
 
-        binding.CloseIdentityButton.setOnClickListener {
-            this.finish()
-        }
+            Column {
+                EnrollmentTitle { this@ZitiEnrollmentActivity.finish() }
 
-        val orienter = object: OrientationEventListener(this, SensorManager.SENSOR_DELAY_NORMAL) {
-            override fun onOrientationChanged(orientation: Int) {
-                binding.MainSelectorLayout.orientation =
-                    if ((orientation in 45..134 || orientation in 225..314))
-                        LinearLayout.HORIZONTAL
-                    else
-                        LinearLayout.VERTICAL
+                EnrollmentCard(R.drawable.jwt, R.string.keyaction) {
+                    jwtEnroll(jwtLauncher)
+                }
+                EnrollmentCard(R.drawable.qr, R.string.qraction) {
+                    qrEnroll(scanLauncher)
+                }
+                EnrollmentCard(R.drawable.link, R.string.enroll_with_url) {
+                    urlDialog.value = true
+                }
             }
         }
-        orienter.enable()
     }
 
     private fun jwtEnroll(launcher: ActivityResultLauncher<Intent>) {
@@ -158,4 +191,133 @@ class ZitiEnrollmentActivity : AppCompatActivity() {
 //                }
 //                .show()
 //    }
+}
+
+@Composable
+fun EnrollmentTitle(onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.End) {
+        Text(
+            stringResource(R.string.add_identity),
+            textAlign = TextAlign.Center,
+            style = TextStyle(
+                color = colorResource(R.color.colorTitle),
+                fontFamily = FontFamily(Font(R.font.russo_one)),
+                fontSize = 26.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .weight(1.0F)
+        )
+
+        Image(
+            painter = painterResource(R.drawable.close),
+            contentDescription = stringResource(R.string.esc),
+            modifier = Modifier
+                .padding(8.dp)
+                .align(Alignment.CenterVertically)
+                .clickable { onClick() }
+        )
+    }
+}
+
+@Composable
+fun EnrollmentCard(imageId: Int, textId: Int, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier.padding(4.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = colorResource(R.color.cardview_light_background)
+        ),
+        border = BorderStroke(1.dp, Color.Black),
+        onClick = onClick
+    ) {
+        val modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .align(Alignment.CenterHorizontally)
+        Image(
+            modifier = modifier.size(100.dp),
+            painter = painterResource(imageId),
+            contentScale = ContentScale.Fit,
+            contentDescription = stringResource(textId)
+        )
+        Text(
+            stringResource(textId),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold),
+            textAlign = TextAlign.Center)
+    }
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun URLDialog(onConfirm: (str: String?) -> Unit) {
+    val clippy = LocalClipboardManager.current
+    var url by remember { mutableStateOf("") }
+    var urlError by remember { mutableStateOf(false) }
+
+    AlertDialog(
+        modifier = Modifier.fillMaxWidth(0.92f),
+        properties = DialogProperties(
+            usePlatformDefaultWidth = false,
+            decorFitsSystemWindows = true,
+            dismissOnClickOutside = true,
+            dismissOnBackPress = true
+        ),
+        shape = RoundedCornerShape(20.dp),
+        onDismissRequest = { onConfirm(null) },
+        confirmButton = {
+            TextButton(
+                onClick = { onConfirm(url) },
+                enabled = url.isNotBlank() && !urlError
+            ) {
+                Text(text = "Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = { onConfirm(null) }) {
+                Text(text = "Cancel")
+            }
+        },
+        title = { Text(text = "Enter Controller URL", fontSize = 18.sp) },
+        text = {
+            FlowRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+
+                OutlinedTextField(
+                    value = url,
+                    onValueChange = { txt ->
+                        runCatching { url = txt; URL(txt) }
+                            .onSuccess { urlError = false }
+                            .onFailure { urlError = true }
+                    },
+                    isError = urlError,
+                    label = { Text("Controller URL") },
+                    modifier = Modifier.weight(1F)
+                )
+                clippy.getText()?.let {
+                    if (it.text.startsWith("https://")) {
+                        TextButton(onClick = { url = it.text }) {
+                            Text(text = "Paste URL\nyou copied")
+                        }
+                    }
+                }
+            }
+        })
+}
+
+@Composable
+@Preview
+fun URLDialogPreview() {
+    URLDialog{}
 }
