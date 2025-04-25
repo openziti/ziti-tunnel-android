118eed1a8a634a20571e666f485b99d209ad880b
ekoby <eugene.kobyakov@netfoundry.io>
2025-04-25 10:07:33 -0400

    use latest SDK

--- tunnel/build.gradle.kts
+++ tunnel/build.gradle.kts
@@ -23,7 +23,7 @@ with(gradleLocalProperties(parent!!.projectDir, providers)) {
 
 android {
     namespace = "org.openziti.tunnel"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 26
